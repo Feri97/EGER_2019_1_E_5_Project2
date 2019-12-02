1ae0d136558b45f219ad0ac979a68695f1b1e8ac
mac5562 <55428222+mac5562@users.noreply.github.com>
2019-12-02 19:54:57 +0100

    footer befejezése Signed-off-by: mac5562 <55428222+mac5562@users.noreply.github.com>

---
+++ views/_footer.php
@@ -0,0 +1,9 @@
+<?php if(!defined('APP_VERSION')){exit;}?>
+</main>
+    <footer>
+        <div class="container">
+            <p>&copy; 2019 Lorem ipsum</p>
+        </div>
+    </footer>
+</body>
+</html>
