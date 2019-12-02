ea42ba1acf9b9013f5edbba45f79394a4cdcfb2a
mac5562 <55428222+mac5562@users.noreply.github.com>
2019-12-02 20:31:27 +0100

    funkciók (Átirányítás,hibakijelzés,posztoló) hozzáadása
    Signed-off-by: mac5562 <55428222+mac5562@users.noreply.github.com>

--- functions.php
+++ functions.php
@@ -16,4 +16,37 @@ function asset($asset) {
     return DOMAIN . $asset;
 }
 
+function url($page = 'home', $params = []) {
+    $url = DOMAIN . "?p={$page}";
+    if (is_array($params)) {
+        foreach ($params as $key => $value) {
+            $url .= "&$key=$value";
+        }
+    }
+    return $url;
+}
+
+function redirect($page = 'home', $params = []) {
+    $url = url($page, $params);
+    header("Location: $url");
+    die();
+}
+
+function is_post() {
+    return $_SERVER['REQUEST_METHOD'] == 'POST';
+}
+
+function html_errors($key) {
+    global $errors;
+
+    $html = "";
+    if (isset($errors[$key])) {
+        foreach ($errors[$key] as $error) {
+            $html .= "<p class='input-error'>$error</p>";
+        }
+    }
+    return $html;
+}
+
+
 ?>
