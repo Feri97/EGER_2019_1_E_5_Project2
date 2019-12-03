89958a93b05c53388f9db5cc60f75dab9e02a11c
Feri97 <csongradi0706@gmail.com>
2019-12-03 22:25:17 +0100

    upload.php - validáció

--- views/upload.php
+++ views/upload.php
@@ -1,5 +1,58 @@
 <?php if(!defined('APP_VERSION')){exit;}?>
 
+<?php 
+    $errors = [];
+    if (is_post()){
+        $title = $_POST['title'];
+        $year = $_POST['year'];
+        $artist = $_POST['artist'];
+        $max_string_length = 255;
+
+        if ($title == null){
+            $errors['title'][] = "Album title is required";
+        }else{
+            if(strlen($title) <= 2 || $max_string_length < strlen($title)){
+                $errors['title'][] = "Album title must be between 3 and 255";
+            }
+        }
+        if ($artist == null){
+            $errors['artist'][] = "Artist is required";
+        }else{
+            if($max_string_length < strlen($artist)){
+                $errors['artist'][] = "Artist must be shorter than 255";
+            }
+        }
+        if ($year == null){
+            $errors['year'][] = "Year is required";
+        } else{
+            if(!is_numeric($year)){
+                $errors['year'][] = "Year must be a number";
+            }
+            else{
+                $curr_year = date('Y');
+                if ($curr_year < $year || $year < 1900){
+                    $errors['year'][] = "Invalid year (must be between 1900-$curr_year)";
+                }
+            }
+        }
+        
+    if(count($errors)==0){
+        
+        $sql = $db->prepare("INSERT INTO `albums` (`artist`,`year`,`title`,`user_id`) values (?, ?, ?, ?)");
+        $sql->bind_param('sisi',  $artist, $year, $title, $_SESSION["id"]);
+        $sql->execute();
+        $sql->close();
+        
+        $new_id = $db->insert_id;
+        
+        redirect('edit', ['id' => $new_id, 'success' => 1]);
+        
+    }
+
+
+    }
+
+?>
 
 <?php include_once "_header.php";?>
 <div class="page page-upload">
