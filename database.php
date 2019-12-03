2cf9c1d1e631d4ba341a3782452fce335842b638
mac5562 <55428222+mac5562@users.noreply.github.com>
2019-12-03 11:21:06 +0100

    database.php (getConnection és getRecord hozzáadása) Signed-off-by: mac5562 <55428222+mac5562@users.noreply.github.com>

---
+++ database.php
@@ -0,0 +1,25 @@
+<?php
+function getConnection() {
+    $connection = new PDO(DB_TYPE.':host='.DB_HOST.';dbname='.DB_NAME.';',DB_USER, DB_PASS);
+    $connection->exec("SET NAMES '".DB_CHARSET."'");
+    return $connection;
+}
+
+function getRecord($queryString, $queryParams = []){
+    // Csatlakozunk az adatbázisunkhoz.
+    $connection = getConnection();  
+    // A paraméterben kapott queryStringből készítek egy utasítást
+    $statement = $connection->prepare($queryString);
+    // Végrehajtjuk az utasítást a megadott paraméterekkel
+    $success = $statement->execute($queryParams);
+    // Ha az utasításunk sikeresen lefutott, akkor a választ olvassuk és tároljuk
+    $result = $success ? $statement->fetch() : [];
+    // Lezárom az utasítás végrehajtását
+    $statement->closeCursor();
+    // Lezárom a kapcsolatot
+    $connection = null;
+    return $result;
+}
+
+
+?>
