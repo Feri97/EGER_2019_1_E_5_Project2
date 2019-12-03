80cad7e9960451ce24e670914aed5d28054d5f74
Feri97 <csongradi0706@gmail.com>
2019-12-03 10:49:06 +0100

    upload-audio.js - then ág

--- js/upload-audio.js
+++ js/upload-audio.js
@@ -12,7 +12,15 @@ document
             document.getElementById("upload-response").innerHTML = ""
 
             axios.post(form.action, formData)
-                .then(function (response) {})
+                .then(function (response) {
+                    let successHTML = `
+                        <div class="success">
+                            <p>${response.data.message}</p>
+                            <p>${response.data.filename}</p>
+                        </div>
+                    `
+                    document.getElementById("upload-response").innerHTML = successHTML
+                })
                 .catch(function (error) {}).finally(function () {
                     enableInputs(inputs)
                 })
