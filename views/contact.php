b1bfbac2f312e4a7403c52b643c6a7089c8f5f5b
peti979797 <peti979797@gmail.com>
2019-12-03 08:38:52 +0100

    contact.php feltöltve

---
+++ views/contact.php
@@ -0,0 +1,15 @@
+<?php if(!defined('APP_VERSION')){exit;}?>
+<?php include_once "_header.php";?>
+<div class="page page-contact">
+       <h1>Contact us</h1>
+       <form class="form-group" method="post">
+       		<input type="text" class="form-input" placeholder="Your name">
+       		<input type="email" class="form-input" placeholder="Your email">
+       		<input type="text" class="form-input" placeholder="Your phone number">
+       		<textarea class="form-input" placeholder="Your message"></textarea>
+       		<input type="submit" class="form-input" value="Send">
+
+       </form>
+         
+</div>
+<?php include_once "_footer.php";?>
