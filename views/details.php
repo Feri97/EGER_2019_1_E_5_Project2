cd756a528ddb0945ebdf358074fbd96e4bda0203
mac5562 <55428222+mac5562@users.noreply.github.com>
2019-12-03 09:09:23 +0100

    details.php (JPlayer lejátszó és a hozzá szükséges jquerry és html dolgok) feltöltése
    Signed-off-by: mac5562 <55428222+mac5562@users.noreply.github.com>

--- views/details.php
+++ views/details.php
@@ -58,3 +58,173 @@ function convert($type,$data){
 	
 
     ?>
+
+
+    <?php include_once "_header.php"; ?>
+
+
+
+
+<div class="page page-details">
+    <table><td><tr>
+    <?php if($album['cover']) : ?>
+        <img src="<?= asset("img/uploads/{$album['cover']}"); ?>" alt="<?php echo $album['title']; ?>">
+    <?php else : ?>
+        <img src="https://via.placeholder.com/300" alt="<?php echo $album['title']; ?>">
+    <?php endif; ?>
+    <h1> <?php echo $album['artist']; ?> - <?php echo $album['title']; ?></h1>
+    </tr></td><td>
+        <tr><a href="<?php echo url(); ?>" style="font-size:22px;"><i class="fa fa-caret-left"></i></a>
+        </tr>
+        <?php if($_SESSION["id"] == select_album_col($id)['user_id']) : ?>
+        <tr><a href="<?php echo url('edit', ['id' => $album['id']]); ?>" style="font-size:20px;" alt="Edit album"><i class="fa fa-cog"></i></a>
+        </tr>
+        <?php endif; ?>
+    </td></table>
+</div>
+<div class="player">
+<div id="jquery_jplayer_1" class="jp-jplayer"></div>
+<div id="jp_container_1" class="jp-audio" role="application" aria-label="media player">
+    <div class="jp-type-playlist">
+        <div class="jp-gui jp-interface">
+            <div class="jp-controls">
+                <button class="jp-previous" role="button" tabindex="0">previous</button>
+                <button class="jp-play" role="button" tabindex="0">play</button>
+                <button class="jp-next" role="button" tabindex="0">next</button>
+                <button class="jp-stop" role="button" tabindex="0">stop</button>
+            </div>
+            
+            <div class="jp-progress">
+                <div class="jp-seek-bar">
+                    <div class="jp-play-bar"></div>
+                </div>
+            </div>
+            <div class="jp-volume-controls">
+                <button class="jp-mute" role="button" tabindex="0">mute</button>
+                <button class="jp-volume-max" role="button" tabindex="0">max volume</button>
+                <div class="jp-volume-bar">
+                    <div class="jp-volume-bar-value"></div>
+                </div>
+            </div>
+            
+            <div class="jp-time-holder">
+                <div class="jp-current-time" role="timer" aria-label="time">&nbsp;</div>
+                <div class="jp-duration" role="timer" aria-label="duration">&nbsp;</div>
+            </div>
+            <div class="jp-toggles">
+                <button class="jp-repeat" role="button" tabindex="0">repeat</button>
+                <button class="jp-shuffle" role="button" tabindex="0">shuffle</button>
+            </div>
+        </div>
+        <div class="jp-playlist">
+            <ul>
+                <li>&nbsp;</li>
+            </ul>
+        </div>
+        
+    
+        
+    </div>
+
+
+        <div class="jp-no-solution">
+            <span>Update Required</span>
+            To play the media you will need to either update your browser to a recent version or update your <a href="http://get.adobe.com/flashplayer/" target="_blank">Flash plugin</a>.
+        </div>
+
+        <div class="jp-title" aria-label="title">&nbsp;</div>
+    </div>
+      
+    
+</div>
+
+
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/jquery.jplayer.min.js"></script>
+<script type="text/javascript" src="js/jplayer.playlist.min.js"></script>
+<script type="text/javascript">
+//<![CDATA[
+
+
+    var theTitles =<?php echo json_encode($namelist);?>;
+    var theMP3s =<?php echo json_encode($datalist);?>;
+    var jplayer_playlist;
+    var isshuffle=false;
+    //var currTitle;
+
+  console.log(theTitles[1]);
+
+  function get_playlist()
+    {
+        var playlist = new Array();
+
+        for(var i = 0; i < theTitles.length; i++)
+        {
+            playlist[i] = {title: theTitles[i], mp3: theMP3s[i]};
+        }
+        return playlist;
+    }
+
+
+    
+    function hideSave()
+    {
+        var x = document.getElementById("save");
+  if (x.style.display === "table") {
+    x.style.display = "none";
+    isshuffle=false;
+    //console.log(isshuffle);
+  } else {
+    x.style.display = "table";
+    isshuffle=true;
+    //console.log(isshuffle);
+  }
+    }
+
+
+
+$(document).ready(function(){
+
+
+
+    
+
+    var playlist = get_playlist();
+        jplayer_playlist = new jPlayerPlaylist({
+            jPlayer: "#jquery_jplayer_1",
+            cssSelectorAncestor: "#jp_container_1",
+            oggSupport:false
+    }, 
+        
+    playlist,{
+        swfPath: "../../dist/jplayer",
+        supplied: "oga, mp3",
+        wmode: "window",
+        useStateClassSkin: true,
+        autoBlur: true,
+        enableRemoveControls: true,
+        smoothPlayBar: true,
+        keyEnabled: true
+        
+    });
+
+
+$("#test").click(function(){
+
+console.log(isshuffle);
+
+    if (isshuffle!=false) {
+
+        jplayer_playlist.shuffle(false);
+        
+    }
+    });
+    
+});
+
+
+
+
+
+//]]>
+</script>
