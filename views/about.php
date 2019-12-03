b18f38e39347ace1ae254542ade0d8a56002d6fa
peti979797 <peti979797@gmail.com>
2019-12-03 08:39:27 +0100

    about.php feltöltve

---
+++ views/about.php
@@ -0,0 +1,19 @@
+<?php if(!defined('APP_VERSION')){exit;}?>
+<?php include_once "_header.php";?>
+<div class="page page-about">  
+
+       <h1 style="font-size:50px; line-height:150%">About us</h1>
+       <img src="image/aboutus.jpg" style="width:100%">
+       <p style="line-height: 150%;">
+            With our application, it's easy to find the right music for every moment - on your phone, your
+            computer, your tablet and more. There are plenty of tracks and episodes. So whether you're
+            behind the wheel, working out, partying or relaxing, the right music is always at your 
+             fingertips. Choose what you want to listen to, or let us surprise you.
+            You can also browse through the collections of friends,     artists, and celebrities.
+            Soundtrack your life with us. Listen for free.
+        </p>
+         
+</div>
+
+
+<?php include_once "_footer.php";?>
