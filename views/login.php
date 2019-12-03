e3b7cda78bda49d5a0243ab69677cc0dd02a1958
Feri97 <csongradi0706@gmail.com>
2019-12-03 08:51:03 +0100

    login.php - form kezelés, validáció

--- views/login.php
+++ views/login.php
@@ -1,5 +1,73 @@
 <?php if(!defined('APP_VERSION')){exit;}?>
+<?php
  
+ // Check if the user is already logged in, if yes then redirect him to welcome page
+ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+     redirect();
+     exit;
+ }
+  
+ require_once "config.php";
+  
+ $username = $password = "";
+ $username_err = $password_err = "";
+  
+ if($_SERVER["REQUEST_METHOD"] == "POST"){
+  
+     if(empty(trim($_POST["username"]))){
+         $username_err = "Please enter username.";
+     } else{
+         $username = trim($_POST["username"]);
+     }
+     
+     if(empty(trim($_POST["password"]))){
+         $password_err = "Please enter your password.";
+     } else{
+         $password = trim($_POST["password"]);
+     }
+     
+     if(empty($username_err) && empty($password_err)){
+         $sql = "SELECT id, username, password FROM users WHERE username = ?";
+         
+         if($stmt = $mysqli->prepare($sql)){
+             $stmt->bind_param("s", $param_username);
+             
+             $param_username = $username;
+             
+             if($stmt->execute()){
+                 $stmt->store_result();
+                 
+                 // Check if username exists, if yes then verify password
+                 if($stmt->num_rows == 1){                    
+                     $stmt->bind_result($id, $username, $hashed_password);
+                     if($stmt->fetch()){
+                         if(password_verify($password, $hashed_password)){
+                             session_start();
+                             
+                             $_SESSION["loggedin"] = true;
+                             $_SESSION["id"] = $id;
+                             $_SESSION["username"] = $username;                            
+                             
+                             redirect();
+                         } else{
+                             $password_err = "The password you entered was not valid.";
+                         }
+                     }
+                 } else{
+                     $username_err = "No account found with that username.";
+                 }
+             } else{
+                 echo "Oops! Something went wrong. Please try again later.";
+             }
+         }
+         
+         $stmt->close();
+     }
+     
+     $mysqli->close();
+ }
+ ?>
+  
 <?php include_once "_header.php";?>
     <div class="page page-login">
         <h1>Login</h1>
