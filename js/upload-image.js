0619b2f273b54aaa6db0e56f373f3710c6a957fc
mac5562 <55428222+mac5562@users.noreply.github.com>
2019-12-03 11:14:26 +0100

    upload-image.js befejezése
    Signed-off-by: mac5562 <55428222+mac5562@users.noreply.github.com>

--- js/upload-image.js
+++ js/upload-image.js
@@ -10,6 +10,43 @@ window.onload = function() {
             let inputs = document.querySelectorAll("form input, form button, form textarea")
             disableInputs(inputs)
 
+				document.getElementById("upload-response").innerHTML = ""
+
+            axios.post(form.action, formData)
+                .then(function (response) {
+                    let successHTML = `
+                        <div class="success">
+                            <p>${response.data.message}</p>
+                            <p><img src="${response.data.image_url}" width="100" alt=""></p>
+                        </div>
+                    `
+                    document.getElementById("upload-response").innerHTML = successHTML
+                })
+                .catch(function (error) {
+                    let errorHTML = ""
+                    const errorMessages = error.response.data.errors
+                    for(let i = 0; i < errorMessages.length; i++) {
+                        errorHTML += `<p class="form-error">${errorMessages[i]}</p>`
+                    }
+                    document.getElementById("upload-response").innerHTML = errorHTML
+                }).finally(function () {
+                    enableInputs(inputs)
+                })
+
+
 
                })
+}
+
+
+function disableInputs(inputs) {
+    for (let i = 0; i < inputs.length; i++) {
+        inputs[i].disabled = true
+    }
+}
+
+function enableInputs(inputs) {
+    for (let i = 0; i < inputs.length; i++) {
+        inputs[i].disabled = false
+    }
 }
