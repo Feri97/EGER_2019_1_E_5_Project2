c96f76cd9fd23535ff2e0f123d91de15e3f41b5c
mac5562 <55428222+mac5562@users.noreply.github.com>
2019-12-03 10:58:10 +0100

    views/upload-cover.php (képfeltöltés és ellenörzés hozzáadása)
    Signed-off-by: mac5562 <55428222+mac5562@users.noreply.github.com>

--- views/upload-cover.php
+++ views/upload-cover.php
@@ -23,5 +23,31 @@ if ($album == null) {
 $errors = [];
 
 
+if (!isset($_FILES['image']) || $_FILES['image']['size'] <= 0) {
+    db_close();
+    http_response_code(400);
+    $response_array = [
+        'errors' => [ 'Cover image is required' ]
+    ];
+    $response = json_encode($response_array);
+    die($response);
+}
 
+$target_dir = BASE_PATH . "/img/uploads/";
+$target_file = $target_dir . basename($_FILES['image']['name']);
+$imageFileType = strtolower(
+    pathinfo($target_file, PATHINFO_EXTENSION)
+);
+
+
+//NOTE: feltöltött fájl kép
+$check = getimagesize($_FILES['image']['tmp_name']);
+if ($check === false) {
+    $errors[] = "Selected file is not an image. File type: {$check['mime']}.";
+}
+
+//NOTE: feltöltött kép létezik
+if (file_exists($target_file)) {
+    $errors[] = "The selected file already exists.";
+}
 ?>
